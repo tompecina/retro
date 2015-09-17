cb7488c5fa74b7faa04caccd532bc7dce8e9587d
Tomáš Pecina <tomas@pecina.cz>
2015-09-17 18:01:45 +0200

    Ondra started

---
+++ src/cz/pecina/retro/ondra/Constants.java
@@ -0,0 +1,64 @@
+/* Constants.java
+ *
+ * Copyright (C) 2015, Tomáš Pecina <tomas@pecina.cz>
+ *
+ * This file is part of cz.pecina.retro, retro 8-bit computer emulators.
+ *
+ * This application is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.         
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cz.pecina.retro.ondra;
+
+/**
+ * Constants specific to Tesla Ondra SPO 186.
+ *
+ * @author @AUTHOR@
+ * @version @VERSION@
+ */
+public final class Constants {
+    
+  /**
+   * Number of PMD 85 models.
+   */
+  public static final int NUMBER_MODELS = 4;
+
+  /**
+   * Array of PMD 85 model strings.
+   */
+  public static final String[] MODELS =
+    {"PMD 85-1", "PMD 85-2", "PMD 85-2A", "PMD 85-3"};
+
+  /**
+   * The default model.
+   */
+  public static final int DEFAULT_MODEL = 3;  // PMD 85-3
+
+
+  /**
+   * The sound sampling rate.
+   */
+  public static int SOUND_SAMPLING_RATE = 16000;
+
+  public static final double CPU_FREQUENCY = 2.048e6;
+  public static final int TIMER_PERIOD = 50;  // in msec
+  public static final long TIMER_CYCLES =
+    Math.round(TIMER_PERIOD * CPU_FREQUENCY / 1e3);
+
+  public static final String RES_PREFIX = "cz/pecina/retro/pmd85/";
+
+  public static final int TAPE_SAMPLE_RATE = 2048000;
+
+  // default constructor disabled
+  private Constants() {}
+}
---
+++ src/cz/pecina/retro/ondra/Emulator.java
@@ -0,0 +1,113 @@
+/* Emulator.java
+ *
+ * Copyright (C) 2015, Tomáš Pecina <tomas@pecina.cz>
+ *
+ * This file is part of cz.pecina.retro, retro 8-bit computer emulators.
+ *
+ * This application is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.         
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cz.pecina.retro.ondra;
+
+import java.util.logging.Logger;
+
+import javax.swing.ToolTipManager;
+
+import cz.pecina.retro.common.GeneralConstants;
+import cz.pecina.retro.common.GeneralUserPreferences;
+import cz.pecina.retro.common.Parameters;
+import cz.pecina.retro.common.Application;
+
+import cz.pecina.retro.memory.Memory;
+
+import cz.pecina.retro.trec.TapeRecorder;
+
+import cz.pecina.retro.peripherals.Peripherals;
+
+import cz.pecina.retro.gui.GUI;
+import cz.pecina.retro.gui.IconCache;
+
+/**
+ * Main class of the application.
+ *
+ * @author @AUTHOR@
+ * @version @VERSION@
+ */
+public class Emulator {
+
+  // // static logger
+  // private static final Logger log =
+  //   Logger.getLogger(Emulator.class.getName());
+
+  // /**
+  //  * Creates an instance of the emulator.
+  //  */
+  // public Emulator() {
+  //   log.fine("New Emulator creation started");
+
+  //   // set OpenGL for Linux environment
+  //   System.setProperty("sun.java2d.opengl",
+  // 		       Parameters.openGL ? "true" : "false");
+  //   // set general parameters
+  //   Parameters.CPUFrequency = Constants.CPU_FREQUENCY;
+  //   Parameters.timerPeriod = Constants.TIMER_PERIOD;
+  //   Parameters.timerCycles = Constants.TIMER_CYCLES;
+  //   Parameters.charset = new OndraCharset();
+
+  //   // initialize application
+  //   GeneralUserPreferences.setNodeClass(UserPreferences.class);
+  //   UserPreferences.getPreferences();
+  //   Application.addModules(new GUI(),
+  //   			   new Memory(),
+  //   			   new TapeRecorder(),
+  //   			   new Peripherals());
+  //   GUI.setPixelSize(UserPreferences.getPixelSize());
+  //   log.fine("Application set up");
+
+  //   // set up tooltip parameters
+  //   final ToolTipManager ttm = ToolTipManager.sharedInstance();
+  //   ttm.setInitialDelay(GeneralConstants.TOOL_TIP_INITIAL_DELAY);
+  //   ttm.setDismissDelay(GeneralConstants.TOOL_TIP_DISMISS_DELAY);
+  //   ttm.setReshowDelay(GeneralConstants.TOOL_TIP_RESHOW_DELAY);
+  //   log.fine("General tooltip options set");
+
+  //   // set up application icons
+  //   for (int size: GUI.APPLICATION_ICON_SIZES) {
+  //     GUI.addApplicationIcon(IconCache.get("ondra/ApplicationIcons/icon-" +
+  //   					   size + ".png").getImage());
+  //   }
+
+  //   // create new computer control object
+  //   new Computer();
+
+  //   log.fine("New Emulator creation completed");
+  // }
+
+  // // for description see Object
+  // @Override
+  // public String toString() {
+  //   return "Emulator";
+  // }
+
+  // /**
+  //  * Main method.
+  //  *
+  //  * @param args command-line arguments
+  //  */
+  // public static void main(final String args[]) {
+  //   log.fine("Application started");
+  //   Parameters.arguments = args;
+  //   new Emulator();
+  // }
+}
