fadc9490dedc4a57782f9c9d0ebbd81207a747d6
Tomáš Pecina <tomas@pecina.cz>
2015-09-19 14:07:30 +0200

    interim commit

--- src/cz/pecina/retro/ondra/DisplayStripe.java
+++ src/cz/pecina/retro/ondra/DisplayStripe.java
@@ -130,13 +130,13 @@ public class DisplayStripe extends JComponent implements Resizeable {
     }
     final int pixelSize = GUI.getPixelSize();
     int p = pixels[row][column];
-    for (int i = 0; i < 6; i++) {
-      graphics.setColor(((p & 1) == 1) ? Color.WHITE : Color.BLACK);
-      graphics.fillRect(pixelSize * ((column * 6) + i),
+    for (int i = 0; i < 8; i++) {
+      graphics.setColor(((p & 0x80) != 0) ? Color.WHITE : Color.BLACK);
+      graphics.fillRect(pixelSize * ((column * 8) + i),
 			pixelSize * row,
 			pixelSize,
 			pixelSize);
-      p >>= 1;
+      p <<= 1;
     }
     log.finest("Cell repainted");
   }
