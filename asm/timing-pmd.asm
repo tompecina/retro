c192cb815cc13c44708048fbb3953c6f590297d2
Tomáš Pecina <tomas@pecina.cz>
2015-10-01 09:44:59 +0200

    interim commit

--- asm/timing-pmd.asm
+++ asm/timing-pmd.asm
@@ -1,33 +1,48 @@
 	.cpu 8080
 	org 3000h
 
-c1:	equ	5dh
-ct:	equ	5fh
-
-	mvi	a,70h
+c1	equ	5dh
+ct	equ	5fh
+	
+	jmp	start
+sub2:	lda	res
+	ret
+ins:	db	0
+start:	mvi	a,70h
 	out	ct
 	mvi	a,0ffh
 	out	c1
+	nop
 	out	c1
-
+	lda	ins
+	mov	d,a
+	lxi	h,field
+	lxi	b,0f00h
+l1:	mov	m,d
+	inx	h
+	dcx	b
+	mov	a,b
+	ora	c
+	jnz	l1
+	mvi	m,0c9h
 	call	read
-	push	h
+	shld	res
+	lxi	h,aux
+	call	field
 	call	read
-	pop	d
-	mov	a,e
-	sub	l
+	xchg
+	lhld	res
+	mov	a,l
+	sub	e
 	mov	l,a
-	mov	a,d
-	sbb	h
+	mov	a,h
+	sbb	d
 	mov	h,a
 	shld    res
 	ret
-
-sub2:	lda	res + 1
-	ret
-	
 read:	mvi	a,40h
 	out	ct
+	nop
 	in	c1
 	mov	l,a
 	in	c1
@@ -35,9 +50,7 @@ read:	mvi	a,40h
 	ret
 
 res:	ds	2
-		
-	mvi	a,55h
-	ret
-
-	end
+aux:	ds	2
+field:	
 	
+	end
