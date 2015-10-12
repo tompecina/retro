16beb290bdf3fefbeb65b5a1374bdaa7c5601461
Tomáš Pecina <tomas@pecina.cz>
2015-10-12 20:53:29 +0200

    interim commit

--- src/cz/pecina/retro/pmd85/Pascal.java
+++ src/cz/pecina/retro/pmd85/Pascal.java
@@ -22,10 +22,6 @@ package cz.pecina.retro.pmd85;
 
 import java.util.logging.Logger;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.util.regex.MatchResult;
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -54,156 +50,6 @@ public class Pascal {
   private static final Logger log =
     Logger.getLogger(Pascal.class.getName());
 
-  // Pascal tokens 0x80-0xff
-  private static String TOKENS[] = {
-    /* 0x80 */ "END", 
-    /* 0x81 */ "FOR",
-    /* 0x82 */ "NEXT",
-    /* 0x83 */ "DATA",
-    /* 0x84 */ "INPUT",
-    /* 0x85 */ "DIM",
-    /* 0x86 */ "READ",
-    /* 0x87 */ "LET",
-    /* 0x88 */ "GOTO",
-    /* 0x89 */ "RUN",
-    /* 0x8a */ "IF",
-    /* 0x8b */ "RESTORE",
-    /* 0x8c */ "GOSUB",
-    /* 0x8d */ "RETURN",
-    /* 0x8e */ "REM",
-    /* 0x8f */ "STOP",
-    /* 0x90 */ "BIT",
-    /* 0x91 */ "ON",
-    /* 0x92 */ "NULL",
-    /* 0x93 */ "WAIT",
-    /* 0x94 */ "DEF",
-    /* 0x95 */ "POKE",
-    /* 0x96 */ "PRINT",
-    /* 0x97 */ "ERR",
-    /* 0x98 */ "LIST",
-    /* 0x99 */ "CLEAR",
-    /* 0x9a */ "LLIST",
-    /* 0x9b */ "RAD",    // MONIT in PMD 85-1
-    /* 0x9c */ "NEW",
-    /* 0x9d */ "TAB(",
-    /* 0x9e */ "TO",
-    /* 0x9f */ "FNC",
-    /* 0xa0 */ "SPC(",
-    /* 0xa1 */ "THEN",
-    /* 0xa2 */ "NOT",
-    /* 0xa3 */ "STEP",
-    /* 0xa4 */ "+",
-    /* 0xa5 */ "-",
-    /* 0xa6 */ "*",
-    /* 0xa7 */ "/",
-    /* 0xa8 */ "^",
-    /* 0xa9 */ "AND",
-    /* 0xaa */ "OR",
-    /* 0xab */ ">",
-    /* 0xac */ "=",
-    /* 0xad */ "<",
-    /* 0xae */ "SGN",
-    /* 0xaf */ "INT",
-    /* 0xb0 */ "ABS",
-    /* 0xb1 */ "USR",
-    /* 0xb2 */ "FRE",
-    /* 0xb3 */ "INP",
-    /* 0xb4 */ "POS",
-    /* 0xb5 */ "SQR",
-    /* 0xb6 */ "RND",
-    /* 0xb7 */ "LOG",
-    /* 0xb8 */ "EXP",
-    /* 0xb9 */ "COS",
-    /* 0xba */ "SIN",
-    /* 0xbb */ "TAN",
-    /* 0xbc */ "ATN",
-    /* 0xbd */ "PEEK",
-    /* 0xbe */ "LEN",
-    /* 0xbf */ "STR$",
-    /* 0xc0 */ "VAL",
-    /* 0xc1 */ "ASC",
-    /* 0xc2 */ "CHR$",
-    /* 0xc3 */ "LEFT$",
-    /* 0xc4 */ "RIGHT$",
-    /* 0xc5 */ "MID$",
-    /* 0xc6 */ "SCALE",
-    /* 0xc7 */ "PLOT",
-    /* 0xc8 */ "MOVE",
-    /* 0xc9 */ "BEEP",
-    /* 0xca */ "AXES",
-    /* 0xcb */ "GCLEAR",
-    /* 0xcc */ "PAUSE",
-    /* 0xcd */ "DISP",
-    /* 0xce */ "_",      // equivalent to ?
-    /* 0xcf */ "BMOVE",
-    /* 0xd0 */ "BPLOT",
-    /* 0xd1 */ "LOAD",
-    /* 0xd2 */ "SAVE",
-    /* 0xd3 */ "DLOAD",
-    /* 0xd4 */ "DSAVE",
-    /* 0xd5 */ "LABEL",
-    /* 0xd6 */ "FILL",
-    /* 0xd7 */ "AUTO",
-    /* 0xd8 */ "OUTPUT",
-    /* 0xd9 */ "STATUS",
-    /* 0xda */ "ENTER",
-    /* 0xdb */ "CONTROL",
-    /* 0xdc */ "CHECK",
-    /* 0xdd */ "CONT",
-    /* 0xde */ "OUT",
-    /* 0xdf */ "INKEY",
-    /* 0xe0 */ "CODE",
-    /* 0xe1 */ "ROM",
-    /* 0xe2 */ "APOKE",
-    /* 0xe3 */ "PEN",
-    /* 0xe4 */ "INK(",
-    /* 0xe5 */ "APEEK",
-    /* 0xe6 */ "ADR",
-    /* 0xe7 */ "AT",
-    /* 0xe8 */ "HEX$",
-    /* 0xe9 */ "DEG",
-    /* 0xea */ "WINDOW",
-    /* 0xeb */ "BEN",
-    /* 0xec */ null,
-    /* 0xed */ null,
-    /* 0xee */ null,
-    /* 0xef */ null,
-    /* 0xf0 */ null,
-    /* 0xf1 */ null,
-    /* 0xf2 */ null,
-    /* 0xf3 */ null,
-    /* 0xf4 */ null,
-    /* 0xf5 */ null,
-    /* 0xf6 */ null,
-    /* 0xf7 */ null,
-    /* 0xf8 */ null,
-    /* 0xf9 */ null,
-    /* 0xfa */ null,
-    /* 0xfb */ null,
-    /* 0xfc */ null,
-    /* 0xfd */ null,
-    /* 0xfe */ null,
-    /* 0xff */ null
-  };
-
-  // REM token
-  private static final int REM = 0x8e;
-
-  // DATA token
-  private static final int DATA = 0x83;
-
-  // common ?/_ token
-  private static final int QM = 0xce;
-  
-  // check address against end address
-  private static void check(final int address,
-			    final int endAddress)
-                            throws PascalException {
-    if (address > endAddress) {
-      throw new PascalException("Outside the program area");
-    }
-  }
-
   // PMD 85 charset
   private static final Charset CS = new PMDCharset();
   
@@ -216,10 +62,6 @@ public class Pascal {
   private static String decodeChar(final int b) {
     return CS.decode(ByteBuffer.wrap(new byte[] {(byte)b})).toString();
   }
-
-  // pattern for getting the line number
-  private static final Pattern LINE_NUMBER_PATTERN =
-    Pattern.compile("^(\\d+) *(\\D.*)$");
 
   // detect Pascal version (-1 = no Pascal, 0 = V1.02, 1 = V2.2)
   private static int getPascalVersion(final byte[] ram) {
@@ -297,12 +139,14 @@ public class Pascal {
       cpu.setPC(ENCODE_TOK_START[version]);
       cpu.exec(Integer.MAX_VALUE, 0, breakPoints);
       if ((cpu.getF() & Intel8080A.CF) == 1) {
-	throw new PascalException("Line tokenization failed on line " + lineNumber);
+	throw new PascalException("Line tokenization failed on line " +
+				  lineNumber);
       }
       cpu.setPC(ENCODE_INS_START[version]);
       cpu.exec(Integer.MAX_VALUE, 0, breakPoints);
       if ((cpu.getF() & Intel8080A.CF) == 1) {
-	throw new PascalException("Line insertion failed on line " + lineNumber);
+	throw new PascalException("Line insertion failed on line " +
+				  lineNumber);
       }
     }
     cpu.suspend();
