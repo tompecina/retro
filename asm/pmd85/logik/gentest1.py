0f8e61daeeabcdad1a8e3ab6e54394ef5c98db5b
Tomáš Pecina <tomas@pecina.cz>
2015-12-07 11:36:23 +0100

    interim commit

--- asm/pmd85/logik/gentest1.py
+++ asm/pmd85/logik/gentest1.py
@@ -30,5 +30,5 @@ for i in range(4000):
     c1 = randint(0, CP - 1)
     c2 = randint(0, CP - 1)
     r = match("%005o" % c1, "%005o" % c2)
-    print("\t.byte\t0x%02x\t; %005o %005o (%d.%d)" % ((r[0] << 3) | r[1], c1, c2, r[0], r[1]))
+    print("\t.byte\t0x%02x\t; %005o %005o (%d,%d)" % ((r[0] << 3) | r[1], c1, c2, r[0], r[1]))
     print("\t.word\t0x%04x, 0x%04x" % (c1, c2))
