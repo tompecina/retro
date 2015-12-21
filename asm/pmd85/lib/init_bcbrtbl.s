4341f44b99638b3e975d7d1607e6472052c0b255
Tomáš Pecina <tomas@pecina.cz>
2015-12-21 05:31:32 +0100

    nightly commit

--- asm/pmd85/lib/init_bcbrtbl.s
+++ asm/pmd85/lib/init_bcbrtbl.s
@@ -17,8 +17,6 @@
 ; You should have received a copy of the GNU General Public License
 ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	.include "pmd85.inc"
-	
 	
 ; ==============================================================================
 ; init_bcbrtbl - initialize the bit manipulation tables
